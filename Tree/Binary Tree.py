b36a34e1f4cb7b2566aa866390393997b5218eb1
Hasibul Habib <hasibulhabib1601@gmail.com>
2024-03-16 20:19:57 +0600

    tree functions specifically traversals function

--- Tree/Binary Tree.py
+++ Tree/Binary Tree.py
@@ -1,6 +1,6 @@
 from collections import deque
-
-class Node:
+import time
+class TreeNode:
     def __init__(self, value):
         self.data = value
         self.left = None
@@ -12,7 +12,7 @@ class Tree:
         self.queue = deque() # it'll act like queue
     
     def insert(self, value) -> None:
-        newNode = Node(value)
+        newNode = TreeNode(value)
         if self.root == None:
             self.root = newNode
             self.queue.append(newNode)
@@ -46,6 +46,31 @@ class Tree:
         print(root.data, end=' ')
         self.print_preOrder(root.left)
         self.print_preOrder(root.right)
+    
+    def preorderTraversal(self, root: TreeNode) -> list[int]: # https://leetcode.com/problems/binary-tree-preorder-traversal/
+        stack, preorderList = [], []
+        curNode = root
+
+        while curNode or stack:
+            while curNode:
+                stack.append(curNode)
+                preorderList.append(curNode.data)
+                curNode = curNode.left
+            curNode = stack.pop().right
+        
+        return preorderList
+
+        # stack, preorderList = [root] if root else [], []
+
+        # while stack:
+        #     curNode = stack.pop()
+        #     preorderList.append(curNode.data)
+        #     if curNode.right:
+        #         stack.append(curNode.right)
+        #     if curNode.left:
+        #         stack.append(curNode.left)
+        
+        # return preorderList
 
     def print_inOrder(self, root) -> None:
         if root == None:
@@ -54,12 +79,44 @@ class Tree:
         print(root.data, end=' ')
         self.print_inOrder(root.right)
     
+    def inorderTraversal(self, root: TreeNode) -> list[int]: # https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+        stack, inorderList = [], []
+        curNode = root
+
+        while curNode or stack:
+            while curNode:
+                stack.append(curNode)
+                curNode = curNode.left
+            curNode = stack.pop()
+            inorderList.append(curNode.data)
+            curNode = curNode.right
+        
+        return inorderList
+    
     def print_postOrder(self, root) -> None:
         if root == None:
             return
         self.print_postOrder(root.left)
         self.print_postOrder(root.right)
         print(root.data, end=' ')
+
+    def postorderTraversal(self, root: TreeNode) -> list[int]: # https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+        stack, postOrderList, visited = [], [], {None}
+        curNode = root
+
+        while curNode or stack:
+            while curNode:
+                stack.append(curNode)
+                curNode = curNode.left
+            
+            curNode = stack[-1]
+            while curNode and (curNode.left in visited and curNode.right in visited):
+                postOrderList.append(stack.pop().data) # stack.pop() == curNode
+                visited.add(curNode)
+                curNode = stack[-1] if stack else None
+            curNode = stack[-1].right if stack else None
+           
+        return postOrderList
     
     def tree_max(self) -> int:
         def max_value_of(root) -> int: # similar to preOrder traversal where root process first, then root.left and root.right
@@ -111,10 +168,11 @@ class Tree:
     
 def main():
     T = Tree()
-    for i in range(1, 16):
+    for i in range(1, 8):
         T.insert(i)
-    # T.queue[0].right = Node(16)
-    # T.queue[-1].right = Node(17)
+    T.root.left.left = None
+    # T.queue[0].right = TreeNode(16)
+    # T.queue[-1].right = TreeNode(17)
     # T.print_preOrder(T.root);  print()
     # T.print_inOrder(T.root);   print()
     # T.print_postOrder(T.root); print()
@@ -124,6 +182,9 @@ def main():
     # print(T.total_leaf_nodes())
     # print(T.is_exist(19))
     # print(T.is_perfect_binary_tree())
+    # print(T.inorderTraversal(T.root))
+    T.print_postOrder(T.root); print()
+    print(T.postorderTraversal(T.root))
 
 if __name__ == '__main__':
     main()
