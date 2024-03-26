8619bc05e036b9639fae14c185f8ae6d10e39b6b
Hasibul Habib <hasibulhabib1601@gmail.com>
2024-03-27 02:33:25 +0600

    good q

--- Tree/Binary Tree.py
+++ Tree/Binary Tree.py
@@ -1,5 +1,5 @@
 from collections import deque
-import time
+from typing import List
 class TreeNode:
     def __init__(self, value):
         self.val = value
@@ -421,6 +421,16 @@ class Tree:
             prevQ, leftToRight = curQ, not leftToRight
         
         return ans
+    
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode: #https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+        if not preorder or not inorder:
+            return None
+        
+        root = TreeNode(preorder[0])
+        idx = inorder.index(preorder[0])
+        root.left  = self.buildTree(preorder[1:idx+1], inorder[:idx])
+        root.right = self.buildTree(preorder[idx+1:],  inorder[idx+1:])
+        return root
     
 def main():
     T = Tree()
